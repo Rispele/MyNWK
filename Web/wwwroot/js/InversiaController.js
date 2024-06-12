cb574d128e4cbb531e2d08b4660ff9a8386b9b3d
AltyngulBiktasheva <evgeniachu130801@gmail.com>
2024-06-12 15:47:33 +0500

    Инверисия (мб нерабочая, не могу проверить)

--- Web/wwwroot/js/InversiaController.js
+++ Web/wwwroot/js/InversiaController.js
@@ -7,6 +7,23 @@ function addStyleSheets(href) {
     link.href = href;
 
     head.appendChild(link);
+
+    if (modeButtonPng !== null) {
+        modeButtonPng.src = "/assets/modeButtonInversia.png";
+    }
+    
+    if (cartPng !== null) {
+        cartPng.src = "/assets/cartInversia.png";
+    }
+    
+    if (logoPng !== null) {
+        logoPng.src = "/assets/logoInversia.png";
+    }
+    
+    // if (editPng !== null) {
+    //     editPng[0].src = "/assets/editButtonInversia.png";
+    //     editPng[1].src = "/assets/editButtonInversia.png";
+    // }
     
     mode = true;
 }
@@ -16,6 +33,22 @@ function disableStyleSheets(href) {
     const link = document.getElementById("inversia-true");
 
     head.removeChild(link);
+
+    if (modeButtonPng !== null) {
+        modeButtonPng.src = "/assets/modeButton.png";
+    }
+
+    if (cartPng !== null) {
+        cartPng.src = "/assets/cart.png";
+    }
+    if (logoPng !== null) {
+        logoPng.src = "/assets/logo.png";
+    }
+
+    // if (editPng !== null) {
+    //     editPng[0].src = "/assets/editButton.png";
+    //     editPng[1].src = "/assets/editButton.png";
+    // }
 
     mode=false;
 }
@@ -31,5 +64,10 @@ function switchMode(href) {
 
 const modeButton = document.getElementsByClassName("modeButton");
 modeButton[0].addEventListener('click', () => switchMode("/css/ColorsInversia.css"));
+
+const modeButtonPng = document.getElementById("modeButton");
+const logoPng = document.getElementById("logo");
+const cartPng = document.getElementById("cart");
+// const editPng = document.getElementById("editButton");
 
 let mode = false;
